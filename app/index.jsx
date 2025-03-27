9b0fa892c155aa282fc1d86001236cb28013e360
1Miken29 <mikenv1311@gmail.com>
2025-03-26 20:10:54 -0600

    Correcciones

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,43 @@
-import { Link } from 'expo-router';
-import '../global.css'
-import { Text, TouchableOpacity, View } from "react-native";
+import { useRouter } from "expo-router";
+import "../global.css";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className='text-[#9B47C3] text-[35px] w-[354px] m-2 text-center font-outfit-medium'>
-        ¿Quieres hacer crecer tu negocio?
-      </Text>
-      <TouchableOpacity className='w-[322px] border border-[#C4C4C4] py-[18px] rounded-full'>
-        <Link href="/Propietario/registerP1" className='text-2xl font-outfit-medium text-center'>Inicia como propietario</Link>
-      </TouchableOpacity>
-      <Text className='text-[#9B47C3] text-[35px] w-[354px] m-2 text-center font-outfit-medium'>
-        ¿Quieres reservar un salón?
-      </Text>
-      <TouchableOpacity className='w-[322px] border border-[#C4C4C4] py-[18px] rounded-full'>
-      <Link href="/Cliente/registerC1" className='text-2xl font-outfit-medium text-center'>Inicia como cliente</Link>
-      </TouchableOpacity>
+    <View className="bg-white h-full">
+      <Image
+        source={require("../assets/images/index_img.png")}
+        className="w-full"
+      />
+      <View className="flex flex-col items-center justify-center">
+        <Text className="font-outfit text-2xl mb-4">
+          Bienvenido a EventSpace
+        </Text>
+        <Text className="font-outfit-semibold text-5xl">
+          Tu Salón, El Escenario De
+        </Text>
+        <Text className="font-outfit-semibold text-5xl my-2 text-[#9B47C3]">
+          Grandes Momentos
+        </Text>
+        <TouchableOpacity
+          className="border border-[#C4C4C4] my-2 py-2 px-6 rounded-full active:"
+          onPress={() => router.push("/Propietario/registerP1")}
+        >
+          <Text className="font-outfit-medium text-xl my-2">
+            Registrate en EventSpace
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="border border-[#C4C4C4] my-2 py-2 px-6 rounded-full active:"
+          onPress={() => router.push("/Propietario/signInP")}
+        >
+          <Text className="font-outfit-medium text-xl my-2">
+            Inicia Sesión en EventSpace
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
